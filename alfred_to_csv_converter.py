7411ccb35069bb7d80d0e239d4ab3c8ad03fcaee
Lj <liangjiajiang@gmail.com>
2023-04-17 23:55:10 -0400

    refactor main method.

--- alfred_to_csv_converter.py
+++ alfred_to_csv_converter.py
@@ -5,7 +5,7 @@ import os
 import plistlib
 import sys
 
-FORWARD_SLASH = "/"
+FORWARD_SLASH = os.path.sep
 SNIPPET_HEADER = "name,keyword,snippet,folder\n"
 SNIPPET_EXTRA_HEADER = "snippetkeywordprefix,snippetkeywordsuffix,folder\n"
 SNIPPET_LIST = []
@@ -14,62 +14,57 @@ ALFRED_SNIPPET = "alfredsnippet"
 
 
 def create_snippet_data(content, folder_name):
-    """create the snippet csv data from alfred."""
-    snippet_line_list = [str(content[ALFRED_SNIPPET]["name"].strip()),
-                         str(content[ALFRED_SNIPPET]["keyword"].strip()),
-                         str(content[ALFRED_SNIPPET]["snippet"].strip()),
-                         folder_name]
-    return ",".join(snippet_line_list)
+    """Create the snippet CSV data from an Alfred JSON snippet."""
+    name = content[ALFRED_SNIPPET]["name"].strip()
+    keyword = content[ALFRED_SNIPPET]["keyword"].strip()
+    snippet = content[ALFRED_SNIPPET]["snippet"].strip()
+    return f"{name},{keyword},{snippet},{folder_name}"
 
 
 def create_snippet_extra_data(content, folder_name):
-    """create the snippet csv data from alfred."""
-    snippet_extra_line_list = [str(content["snippetkeywordprefix"].strip()),
-                               str(content["snippetkeywordsuffix"].strip()),
-                               folder_name]
-    return ",".join(snippet_extra_line_list)
+    """Create the snippet CSV data from an Alfred plist snippet."""
+    prefix = content["snippetkeywordprefix"].strip()
+    suffix = content["snippetkeywordsuffix"].strip()
+    return f"{prefix},{suffix},{folder_name}"
 
 
 def process_json_files(root, file):
-    """process json data."""
-    folder_name = str(root).split(FORWARD_SLASH)[-1:].pop()
+    """Process an Alfred JSON snippet file."""
+    folder_name = str(root).split(FORWARD_SLASH)[-1]
     with open(os.path.join(root, file), encoding="utf8") as json_file:
         content = json.load(json_file)
         SNIPPET_LIST.append(create_snippet_data(content, folder_name))
 
 
 def process_plist_files(root, file):
-    """process plist data."""
-    folder_name = str(root).split(FORWARD_SLASH)[-1:].pop()
+    """Process an Alfred plist snippet file."""
+    folder_name = str(root).split(FORWARD_SLASH)[-1]
     with open(os.path.join(root, file), "rb") as plist_file:
         content = plistlib.load(plist_file)
         SNIPPET_EXTRA_LIST.append(create_snippet_extra_data(content, folder_name))
 
 
 def walk_through_folder(folders):
-    """walk through all file under the 'snippet' folder."""
-    for (root, path, files) in os.walk(folders):
+    """Walk through all files under the 'snippet' folder and process them."""
+    for root, _, files in os.walk(folders):
         for file in files:
-            if str(file).endswith(".json"):
+            if file.endswith(".json"):
                 process_json_files(root, file)
-            if str(file).endswith(".plist"):
+            elif file.endswith(".plist"):
                 process_plist_files(root, file)
 
 
 def write(snippet_data, header, file_name):
-    """write to corresponding files."""
+    """Write the snippet data to a CSV file."""
     root = "output"
-    if not os.path.isdir(root):
-        os.mkdir(root)
-    with open(file_name, "w", encoding="utf8") as output:
+    os.makedirs(root, exist_ok=True)
+    with open(os.path.join(root, file_name), "w", encoding="utf8") as output:
         output.write(header)
-        for line in snippet_data:
-            output.write(line)
-            output.write("\n")
+        output.writelines([f"{line}\n" for line in snippet_data])
 
 
 if __name__ == '__main__':
     args = sys.argv
     walk_through_folder(args[1])
-    write(SNIPPET_LIST, SNIPPET_HEADER, "output/snippet.csv")
-    write(SNIPPET_EXTRA_LIST, SNIPPET_EXTRA_HEADER, "output/snippet_extra.csv")
+    write(SNIPPET_LIST, SNIPPET_HEADER, "snippet.csv")
+    write(SNIPPET_EXTRA_LIST, SNIPPET_EXTRA_HEADER, "snippet_extra.csv")
